83d064be02fac80f2de8620061266ec5d77e3bdd
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-03-05 17:42:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
             whiteList.appendChild(contEl);
             var box = document.createElement('input');
             box.type="checkbox"
-            box.className="box15"
+            box.className="box16"
             
              
          box.addEventListener('click', function(){
